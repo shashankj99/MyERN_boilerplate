de3f65c517dea70b3138914d6ff2b4edb27f0bfa
shashankj99 <shashankj677@gmail.com>
2020-04-26 10:24:25 +0545

    added jsonwebtoken and made login function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,9 @@ const db = require('../models');
 const {validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
 const User = db.users;
+const jwt = require('jsonwebtoken');
 const Op = db.Sequelize.Op;
+const sequelize = db.sequelize;
 
 const SALT_ROUNDS = 10;
 
@@ -14,7 +16,7 @@ exports.create = async (req, res, next) => {
         if (errors.isEmpty()) {
             hashedPassword = bcrypt.hashSync(req.body.password, SALT_ROUNDS);
 
-            // create a user instance
+            // create a new user instance
             const user = {
                 first_name: req.body.first_name,
                 last_name: req.body.last_name,
@@ -23,41 +25,81 @@ exports.create = async (req, res, next) => {
                 role: req.body.role
             };
 
+            //begin database transaction
+            const t = await sequelize.transaction();
+
             // save the user in the database
-            await User.create(user).then((data) => {
+            await User.create(user, {transaction: t}).then((data) => {
+                t.commit();
                 res.send(data);
             }).catch((err) => {
+                t.rollback();
                 res.status(500).send({
                     message: err.message || "An error occurred while registering the users"
                 });
             });
+
             return next();
+        } else {
+            const extractedErrors = [];
+            errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
+
+            return res.status(422).json({
+                errors: extractedErrors,
+            });
         }
-
-        const extractedErrors = [];
-        errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
-
-        return res.status(422).json({
-            errors: extractedErrors,
-        });
     } catch (error) {
         return next(error);
     }
 }
 
 // user login
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
     try {
         const errors = validationResult(req);
 
-        const extractedErrors = [];
-        errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
+        if (errors.isEmpty()) {
+            const user = await User.findOne({where: {email: req.body.email}});
+            if (user) {
+                const password = bcrypt.compareSync(req.body.password, user.dataValues.password);
+                if(password) {
+                    getToken = jwt.sign(user.id.toString(16), 'secret');
+                    if (getToken) {
+                        //begin database transaction
+                        const t = await sequelize.transaction();
+                        user.token = getToken;
+                        await user.save({transaction: t});
+                        t.commit();
+                        
+                        res.cookie("x_auth", user.token).status(200).json({"loginSuccess": true});
+                    } else {
+                        t.rollback();
+                        return res.json({
+                            "tokenGeneration": false,
+                            "message": "Failed to generate token for this user"
+                        });
+                    }
+                } else {
+                    return res.json({
+                        "loginSuccess": false,
+                        "message": "Authentication failed, password didn't match"
+                    });
+                }
+            } else {
+                return res.json({
+                    "loginSuccess": false,
+                    "message": "Authentication failed, email not found"
+                });
+            }
+        } else {
+            const extractedErrors = [];
+            errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
 
-        return res.status(422).json({
-            errors: extractedErrors,
-        });
+            return res.status(422).json({
+                errors: extractedErrors,
+            });
+        }
     } catch (error) {
         return next(error);
     }
 }
-
